c9990ee5c084bbee97f2a1fd261fb98c3e0a8a5c
Arkadiusz Błasiak <gmail>
2013-12-23 11:14:59 +0100

    kafelki i przyklejona pilka

--- grafika.pas
+++ grafika.pas
@@ -36,8 +36,8 @@ var
    DirApp  : UTF8String;
    DirHome : UTF8String;
    fullscreen : Boolean;
-   ruch_x : Integer = 100;
-   ruch_y : Integer = 100;
+   ruch_x : Integer;
+   ruch_y : Integer;
    wektor_x, wektor_y : shortint;
 type
    kafelek = record
@@ -46,7 +46,7 @@ type
      kolor : LongWord;
    end;
 VAR
-   kafelki : ARRAY [1..20] of kafelek;
+   kafelki : ARRAY [1..50] of kafelek;
 
 procedure rysuj_pilke(x : Integer; y : Integer);
 begin
@@ -60,7 +60,7 @@ end;
 
 procedure rysuj_kafelek(x : Integer; y: Integer; kolor : LongWord);
 Begin
-  pr2d_Rect( x, y, Ceil(szerokosc/15), Ceil(wysokosc/25), kolor, 255, PR2D_FILL );
+  pr2d_Rect( x, y, Ceil(szerokosc/15), Ceil(wysokosc/28), kolor, 255, PR2D_FILL );
 end;
 
 function IntToBool(liczba : shortint) : Boolean;
@@ -104,20 +104,46 @@ begin
   wektor_y := 1;
   pal_x := 100;
   dlugosc_paletki := Ceil(szerokosc/8);
+  ruch_x := pal_x + Ceil(dlugosc_paletki/2);
+  ruch_y := wysokosc-Ceil(wysokosc/20) - 10;
   //pierwsze pietro kafelkow
   for i := 1 to 10 do begin
   with kafelki[i] do begin
-    pozycja_x := Ceil(szerokosc/15) + (60 * i);
-    pozycja_y := 100;
+    pozycja_x :=  Ceil(szerokosc/15)+(Ceil(szerokosc/15) * i)+5*i;
+    pozycja_y := Ceil(wysokosc/2);
     kolor := $ff0000;
   end;
   end;
   //drugie pietro kafelkow
   for i := 11 to 19 do begin
   with kafelki[i] do begin
-    pozycja_x := (60 * i) - szerokosc + Ceil(szerokosc/2.8);
-    pozycja_y := 150;
+    pozycja_x := Ceil((szerokosc/15)/2)+Ceil(szerokosc/15)+(Ceil(szerokosc/15) * (i-10))+ 5*(i-10);
+    pozycja_y := Ceil(wysokosc/2)-10-Ceil(wysokosc/28);
     kolor := $0000ff;
+  end;
+  end;
+  //trzecie
+  for i := 20 to 29 do begin
+  with kafelki[i] do begin
+    pozycja_x :=  Ceil(szerokosc/15)+(Ceil(szerokosc/15) * (i-19))+5*(i-19);
+    pozycja_y := Ceil(wysokosc/2)-20-Ceil(wysokosc/28)*2;
+    kolor := $00ff00;
+  end;
+  end;
+  //czwarte
+  for i := 30 to 38 do begin
+  with kafelki[i] do begin
+    pozycja_x := Ceil((szerokosc/15)/2)+Ceil(szerokosc/15)+(Ceil(szerokosc/15) * (i-29))+ 5*(i-29);
+    pozycja_y := Ceil(wysokosc/2)-30-Ceil(wysokosc/28)*3;
+    kolor := $ffff00;
+  end;
+  end;
+  //piąte
+  for i := 39 to 48 do begin
+  with kafelki[i] do begin
+    pozycja_x :=  Ceil(szerokosc/15)+(Ceil(szerokosc/15) * (i-38))+5*(i-38);
+    pozycja_y := Ceil(wysokosc/2)-40-Ceil(wysokosc/28)*4;
+    kolor := $00ffff;
   end;
   end;
 end;
@@ -129,10 +155,8 @@ begin
   pr2d_Rect( 0, 0, szerokosc, wysokosc, $FFFFFF, 255, PR2D_FILL );
   rysuj_pilke(ruch_x,ruch_y);
   rysuj_paletke(pal_x);
-  for i := 1 to 10 do
+  for i := 1 to 48 do
     rysuj_kafelek(kafelki[i].pozycja_x,kafelki[i].pozycja_y,kafelki[i].kolor);
-  for i := 11 to 20 do
-     rysuj_kafelek(kafelki[i].pozycja_x,kafelki[i].pozycja_y,kafelki[i].kolor);
 end;
 
 procedure Update( dt : Double );
@@ -149,8 +173,16 @@ procedure Timer_pilka;
 begin
   if (ruch_x = szerokosc-15) or (ruch_x = 15) then wektor_x := wektor_x * (-1);
   if (ruch_y = wysokosc-15) or (ruch_y = 15) then wektor_y := wektor_y * (-1);
-  ruch_x := ruch_x + wektor_x;
-  ruch_y := ruch_y + wektor_y;
+  if (ruch_x <= pal_x+dlugosc_paletki) and (ruch_x >= pal_x) and (ruch_y = wysokosc-Ceil(wysokosc/20)-5) then wektor_y := wektor_y*(-1);
+  //gdy pilka w ruchu
+  if rusz = 1 then begin
+    ruch_x := ruch_x + wektor_x;
+    ruch_y := ruch_y + wektor_y;
+  end
+  //gdy pilka przylepiona do paletki
+  else begin
+    ruch_x := pal_x+Ceil(dlugosc_paletki/2);
+  end;
 end;
 
 procedure Timer_paletka;
